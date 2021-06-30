ce6b0e666377211f22a45cff125d8d67a57ff763
acolagior <acolagior@student.ethz.ch>
2021-06-30 10:26:13 -0400

    New load racing

--- src/dataloader/load_racing_data_from_rosbag.py
+++ src/dataloader/load_racing_data_from_rosbag.py
@@ -83,13 +83,12 @@ def perturbationIMUandBiases(config_fn):
     with rosbag.Bag(bagfile, 'r') as bag:
         for (topic, msg, ts) in bag.read_messages():
             if topic == topic_imu:
-                #IMU Interpolation at 1000 Hz -> IMU_measurements.txt
+                
 
-                # Noise generation on IMU and bias for angular velocity and linear acceleration
-
+                
                 if first:
-                    dt_sqrt = 0
-                    dt_sqrt.append(dt_sqrt)
+                    dt_sqrt_ = 0
+                    dt_sqrt.append(dt_sqrt_)
                     ts_imu.append(msg.header.stamp.to_sec())
 
                     #Angular velocity and Linear Acceleration GT (from simulation)
@@ -101,12 +100,11 @@ def perturbationIMUandBiases(config_fn):
                 else:
                     curr_ts_imu = msg.header.stamp.to_sec()
                     prev_ts_imu = ts_imu[-1]
-                    dt_sqrt = math.sqrt(curr_ts_imu - prev_ts_imu)
-                    dt_sqrt.append(dt_sqrt)
                     dt = curr_ts_imu - prev_ts_imu
+                    dt_sqrt_ = math.sqrt(dt/2)
                     t = prev_ts_imu + dt / 2.0
 
-                    #Calib IMU Interpolation: measurements will be now at 1000 Hz
+                    #IMU Interpolation: measurements will be now at 1000 Hz
                     curr_w_gt = np.array([msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z])
                     curr_a_gt = np.array([msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z])
 
@@ -123,7 +121,12 @@ def perturbationIMUandBiases(config_fn):
                     ts_imu.append(curr_ts_imu)
 
                     w_gt.append(interp_w_gt)
+                    w_gt.append(curr_w_gt)
+                    a_gt.append(interp_a_gt)
                     a_gt.append(curr_a_gt)
+
+                    dt_sqrt.append(dt_sqrt_)
+                    dt_sqrt.append(dt_sqrt_)
 
 
             if topic == topic_odometry:
@@ -138,22 +141,30 @@ def perturbationIMUandBiases(config_fn):
 
 
     for i in range(n_trajectories):
-        print("Creating file with trajectory:", i)
+        print("Creating files for trajectory:", i)
 
+        #IMU Biases generation
         bias_acc = np.random.uniform(bias_acc_min, bias_acc_max, 3) #bias generation using uniform distribution - Lin. Accel.
         bias_w = np.random.uniform(bias_gyro_min, bias_gyro_max, 3) #bias generation using uniform distribution - Ang. Vel.
+        bias_w = np.reshape(bias_w, (1, 3))
+        bias_acc = np.reshape(bias_acc, (1, 3))  
 
+        #Find w and a: calib (GT + IMU noise) and raw (GT + bias + IMU noise + Bias noise)
         w_calib = np.asarray(w_gt)
         a_calib = np.asarray(a_gt)
         w_raw =  np.asarray(w_gt)
         a_raw =  np.asarray(a_gt)
+
+        #Get np arrays
         ts_imu = np.asarray(ts_imu)
         ts_odom = np.asarray(ts_odom)
         p_wb = np.asarray(p_wb)
         q_wb = np.asarray(q_wb)
         v_wb = np.asarray(v_wb)
         dt_sqrt = np.asarray(dt_sqrt)
+        dt_sqrt = np.reshape(dt_sqrt, (21107,1)) #modify number
 
+        # Noise generation on IMU and bias for w and a
         noise_IMU_w = np.random.normal(0,stdImuNoise_gyro,(w_calib.shape[0], w_calib.shape[1])) #mean, std, number elements
         noise_IMU_acc = np.random.normal(0,stdImuNoise_acc,(a_calib.shape[0], a_calib.shape[1]))
         noise_bias_w = np.random.normal(0,stdBiasNoise_gyro,(w_raw.shape[0], w_raw.shape[1]))
@@ -161,12 +172,11 @@ def perturbationIMUandBiases(config_fn):
 
         w_calib = w_calib + noise_IMU_w
         a_calib = a_calib + noise_IMU_acc
+                  
         w_raw = w_raw + noise_IMU_w + bias_w + noise_bias_w* dt_sqrt
         a_raw = a_raw + noise_IMU_acc + bias_acc + noise_bias_acc* dt_sqrt
 
-
-        #Transform lists to arrays
-
+   
         # ================================== EXPORT FILES FOR DATASET GENERATION ==================================
         seq_name = "seq" + str(i+1)
         seq_dir = os.path.join(out_dir, seq_name)
@@ -202,8 +212,10 @@ def perturbationIMUandBiases(config_fn):
         np.savetxt(fn, tableEvolvingState)
 
         # Save biases values: bias.txt
+        biasesValues = np.concatenate((bias_acc,bias_w), axis=1)
+       
 
-        biasesValues = np.concatenate((bias_acc,bias_w), axis=0)
+         #check how are we concatenating
         all_biases.append(np.asarray(biasesValues))
         targetsBiases  = np.array(["Bias_acc_x", "Bias_acc_y", "Bias_acc_z", "Bias_w_x", "Bias_w_y", "Bias_w_z"])
         biasesValues = np.array(biasesValues)
@@ -218,15 +230,12 @@ def perturbationIMUandBiases(config_fn):
     fn = os.path.join(out_dir, "n_sequences.txt")
     np.savetxt(fn, np.array([n_trajectories], dtype=int), fmt='%d')
 
-    #ToDo
     all_biases = np.array(all_biases)
+    all_biases = np.reshape(all_biases, (n_trajectories, 6))
     targetsBiases  = np.array(["Bias_acc_x", "Bias_acc_y", "Bias_acc_z", "Bias_w_x", "Bias_w_y", "Bias_w_z"])
     fn = os.path.join(out_dir, "all_biases.txt")
     np.savetxt(fn, all_biases, header = "Bias_acc_x, Bias_acc_y, Bias_acc_z, Bias_w_x, Bias_w_y, Bias_w_z")
 
-
-
-    # IPython.embed()
 
     return
 
