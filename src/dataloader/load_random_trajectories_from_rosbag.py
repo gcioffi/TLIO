d91be23f5400deb7eff560b39514049d5c633f4b
acolagior <acolagior@student.ethz.ch>
2021-09-19 10:47:32 +0000

    dataloader

--- src/dataloader/load_random_trajectories_from_rosbag.py
+++ src/dataloader/load_random_trajectories_from_rosbag.py
@@ -131,8 +131,8 @@ def perturbationIMUandBiases(config_fn, file, conf, traj_analysed, rosbags_num, 
             if topic == topic_odometry:
                 if first_odom:
                 #Save GT timestamps, pose (position + orientation) and velocity from simulation -> evolving state.txt
-                    # Remove offset VICON - IMU: 0.03 secs
-                    ts_odom.append(msg.header.stamp.to_sec() - 0.03)
+                    # Remove offset sync VICON - IMU: 0.03 secs
+                    ts_odom.append(msg.header.stamp.to_sec() - 0.12)
                     p_wb.append(np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]))
                     q_wb.append(np.array([msg.pose.orientation.w, msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z]))
                     v_wb.append(np.array([0,0,0]))
@@ -141,7 +141,8 @@ def perturbationIMUandBiases(config_fn, file, conf, traj_analysed, rosbags_num, 
                 else: 
              
                     ts_odom_prev = ts_odom[-1]
-                    ts_odom.append(msg.header.stamp.to_sec() - 0.03)
+                    # Remove offset sync VICON - IMU: 0.03 secs
+                    ts_odom.append(msg.header.stamp.to_sec() - 0.12)
                     p_wb_prev = p_wb[-1]
                     p_wb.append(np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]))
                     q_wb.append(np.array([msg.pose.orientation.w, msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z]))
